d382e2b004f51d9150e0f6f061e73bb6fa0ae70f
kleemamn <kleemamn@mail.uc.edu>
2022-10-23 18:44:44 -0400

    Initial commit

---
+++ src/SafeInput.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SafeInput
+{
+    Scanner in = new Scanner(System.in);
+
+    /**
+     *
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return a String response that is not zero length
+     */
+    public static String getNonZeroLenString(Scanner pipe, String prompt)
+    {
+        String retString = "";  // Set this to zero length. Loop runs until it isn’t
+        do
+        {
+            System.out.print("\n" + prompt + ":  "); // show prompt add space
+            retString = pipe.nextLine();
+        }while(retString.length() == 0);
+
+        return retString;
+
+    }
+
+}
